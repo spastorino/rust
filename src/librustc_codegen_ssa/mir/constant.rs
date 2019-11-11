33ddffcfa4f0bcf4f6a79afad616349d23e641ad
Santiago Pastorino <spastorino@gmail.com>
2019-11-11 12:49:21 +0100

    Simplify pattern

--- src/librustc_codegen_ssa/mir/constant.rs
+++ src/librustc_codegen_ssa/mir/constant.rs
@@ -14,8 +14,8 @@ impl<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>> FunctionCx<'a, 'tcx, Bx> {
         constant: &mir::Constant<'tcx>,
     ) -> Result<&'tcx ty::Const<'tcx>, ErrorHandled> {
         match constant.literal.val {
-            mir::interpret::ConstValue::Unevaluated(def_id, ref substs) => {
-                let substs = self.monomorphize(substs);
+            mir::interpret::ConstValue::Unevaluated(def_id, substs) => {
+                let substs = self.monomorphize(&substs);
                 let instance = ty::Instance::resolve(
                     self.cx.tcx(), ty::ParamEnv::reveal_all(), def_id, substs,
                 ).unwrap();
