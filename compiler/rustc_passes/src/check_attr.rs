07ff1f9030a55016bad4565ac1961212fa60b069
Santiago Pastorino <spastorino@gmail.com>
2023-02-01 17:51:45 -0300

    Remove Target::ImplTraitPlaceholder

--- compiler/rustc_passes/src/check_attr.rs
+++ compiler/rustc_passes/src/check_attr.rs
@@ -717,7 +717,6 @@ impl CheckAttrVisitor<'_> {
             | Target::GlobalAsm
             | Target::TyAlias
             | Target::OpaqueTy
-            | Target::ImplTraitPlaceholder
             | Target::Enum
             | Target::Variant
             | Target::Struct
